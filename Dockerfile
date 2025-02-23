46398779b0a46c80a4ca8acc5dfb6534c33a23db
hitesh <hiteshkamble345@gmail.com>
2025-02-23 22:43:26 +0530

    Improvement: used docker multistaged, separate dontainers for app and database

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,31 @@
-FROM ubuntu:latest
+FROM  python:3.12-slim AS installer 
 
-RUN apt update && apt upgrade -y && \
-    apt install -y python3 python3-pip python3.12-venv \
-    pkg-config libmysqlclient-dev mysql-server && \
-    rm -rf /var/lib/apt/lists/*  # Cleanup to reduce image size
+WORKDIR /app
 
-WORKDIR /home/ubuntu
+RUN apt-get update && apt-get install -y \
+    pkg-config default-libmysqlclient-dev \
+    build-essential \
+    && rm -rf /var/lib/apt/lists/*
 
-# Creating a virtual environment and install packages
-RUN python3 -m venv phonebook && \
-    ./phonebook/bin/pip install --upgrade pip && \
-    ./phonebook/bin/pip install flask flask_mysqldb
+COPY requirements.txt .
 
-COPY . ./phonebook
+RUN pip install --no-cache-dir -r requirements.txt
 
-COPY entrypoint.sh /entrypoint.sh
-RUN chmod +x /entrypoint.sh
+
+
+# stage 2
+FROM python:3.12-slim
+
+RUN apt-get update && apt-get install -y \
+    default-mysql-client \
+    && rm -rf /var/lib/apt/lists/*
+
+COPY --from=installer /usr/local/lib/python3.12/site-packages /usr/local/lib/python3.12/site-packages
+
+WORKDIR /app
+
+COPY . .
 
 EXPOSE 5000
 
-CMD ["/entrypoint.sh"]
+CMD ["python3", "app.py"]
--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 from flask import Flask, render_template, request, redirect, flash
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
+import os 
 import sys
+from dotenv import load_dotenv
 
-app = Flask(__name__)
+app= Flask(__name__, template_folder='templates')
 app.secret_key = "your_secret_key"
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'hitesh'
-app.config['MYSQL_PASSWORD'] = 'hitesh'
-app.config['MYSQL_DB'] = 'phonebook'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+load_dotenv()
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_CURSORCLASS'] = os.getenv('MYSQL_CURSORCLASS')
+
+
+print(app.config['MYSQL_HOST'])
+print(app.config['MYSQL_USER'])
+print(app.config['MYSQL_PASSWORD'])
+print(app.config['MYSQL_DB'])
 
 mysql = MySQL(app)
 
@@ -20,6 +30,7 @@ def init_db():
             host=app.config['MYSQL_HOST'],
             user=app.config['MYSQL_USER'],
             passwd=app.config['MYSQL_PASSWORD'],
+            database=app.config['MYSQL_DB'],  # ✅ Explicitly select the DB
             cursorclass=MySQLdb.cursors.DictCursor,
         )
         cursor = conn.cursor()
@@ -72,6 +83,7 @@ def view_contacts():
         cursor = mysql.connection.cursor()
         cursor.execute("SELECT * FROM contacts")
         contacts = cursor.fetchall()
+        print(contacts)
         return render_template('view_contacts.html', contacts=contacts)
     except Exception as e:
         flash(f"Error fetching contacts: {e}", 'error')
